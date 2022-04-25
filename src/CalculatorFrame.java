406c2b99d3d84fabe0a389441b71bbd4b76722f5
Melek <abbassimelek@gmail.com>
2022-04-25 15:00:48 +0100

    same

--- src/CalculatorFrame.java
+++ src/CalculatorFrame.java
@@ -396,9 +396,27 @@ public class CalculatorFrame extends JFrame{
         textField.setText("0");
     }
 
+    // private void jButton1ActionPerformed(java.awt.event.ActionEvent evt) {                                         
+    //     // TODO add your handling code here:
 
 
-    
+
+        
+    // } 
+
+    private void button1ActionPerformed(java.awt.event.ActionEvent evt) {                                         
+        // TODO add your handling code here:
+        String button_text = button1.getText();
+        String screen_text = textField.getText();
+
+        if (screen_text.equals("0")) {
+            textField.setText(button_text);
+        } else {
+            textField.setText(screen_text + button_text);
+        }
+    }
+
+    /*aaaaaaaa*/
     public static void main(String[] args) throws Exception {
         new CalculatorFrame();
     }
